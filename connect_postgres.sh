5b45468903b9b10e59f45b089a488b44016687e6
Alex Liddle <alex37184@yahoo.com>
2018-07-29 16:47:07 -0500

    Update connect_postgres.sh

--- connect_postgres.sh
+++ connect_postgres.sh
@@ -1,2 +1,2 @@
 export PATH=$PATH:/Library/PostgreSQL/9.6/bin
-psql "sslmode=disable dbname=airbnb user=postgres hostaddr=35.226.144.248"
+psql "sslmode=disable dbname=airbnb user=postgres hostaddr=00.000.000.000"
